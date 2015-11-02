4ee91097bf539533d809dafdde556217f8942dae
Amir Kibbar <amir.kibbar@hp.com>
2015-11-02 15:27:22 -0800

    added support for the new download URL of the elastic 2.0.x

--- src/main/groovy/ajk/gradle/elastic/ElasticActions.groovy
+++ src/main/groovy/ajk/gradle/elastic/ElasticActions.groovy
@@ -29,6 +29,12 @@ class ElasticActions {
         println "* elastic: installing elastic version $version"
         String linuxUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz"
         String winUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.zip"
+
+        if (version.startsWith("2")) {
+            linuxUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${version}/elasticsearch-${version}.tar.gz"
+            winUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/${version}/elasticsearch-${version}.zip"
+        }
+
         String elasticPackage = isFamily(FAMILY_WINDOWS) ? winUrl : linuxUrl
         File elasticFile = new File("$toolsDir/elastic-${version}.zip")
 
@@ -52,7 +58,7 @@ class ElasticActions {
             ant.chmod(file: new File("$home/bin/elasticsearch"), perm: "+x")
         }
 
-        if (withPlugins.contains("head plugin")){
+        if (withPlugins.contains("head plugin")) {
             println "* elastic: installing the head plugin"
             String plugin = "$home/bin/plugin"
             if (isFamily(FAMILY_WINDOWS)) {
