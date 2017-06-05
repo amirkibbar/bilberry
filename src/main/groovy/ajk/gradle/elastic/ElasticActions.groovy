2e27ac14b2f8c0c11cb71223824dd5bd24281326
Amir Kibbar <amir.kibbar@tri.global>
2017-06-05 16:00:47 -0700

    added support for elastic version >= 5.0.0 (different download URL)

--- src/main/groovy/ajk/gradle/elastic/ElasticActions.groovy
+++ src/main/groovy/ajk/gradle/elastic/ElasticActions.groovy
@@ -9,121 +9,125 @@ import static org.apache.tools.ant.taskdefs.condition.Os.FAMILY_WINDOWS
 import static org.apache.tools.ant.taskdefs.condition.Os.isFamily
 
 class ElasticActions {
-    String version
-    File toolsDir
-    Project project
-    AntBuilder ant
-    File home
+  String version
+  File toolsDir
+  Project project
+  AntBuilder ant
+  File home
 
-    ElasticActions(Project project, File toolsDir, String version) {
-        this.project = project
-        this.toolsDir = toolsDir
-        this.version = version
-        this.ant = project.ant
-        home = new File("$toolsDir/elastic")
+  ElasticActions(Project project, File toolsDir, String version) {
+    this.project = project
+    this.toolsDir = toolsDir
+    this.version = version
+    this.ant = project.ant
+    home = new File("$toolsDir/elastic")
+  }
+
+  boolean isInstalled() {
+    if (!new File("$toolsDir/elastic-${version}.zip").exists()) return false
+
+    if (!new File("$home/bin/elasticsearch").exists()) return false
+
+    boolean desiredVersion = isDesiredVersion()
+
+    if (!desiredVersion) {
+      // this is not the, then we also need to delete the home directory
+      ant.delete(dir: home)
+
+      return false
     }
 
-    boolean isInstalled() {
-        if(!new File("$toolsDir/elastic-${version}.zip").exists()) return false
+    return true
+  }
 
-        if(!new File("$home/bin/elasticsearch").exists()) return false
+  boolean isDesiredVersion() {
+    def versionInfo = new StringBuffer()
 
-        boolean desiredVersion = isDesiredVersion()
+    println "${CYAN}* elastic:$NORMAL checking existing version"
+    if (isFamily(FAMILY_WINDOWS)) {
+      File versionScript = new File("${home}/bin/version.bat")
 
-        if (!desiredVersion) {
-            // this is not the, then we also need to delete the home directory
-            ant.delete(dir: home)
+      if (!versionScript.exists()) {
+        ant.copy(file: "${home}/bin/elasticsearch.bat", tofile: versionScript, filtering: true)
+        ant.replace(file: versionScript, token: "org.elasticsearch.bootstrap.Elasticsearch", value: "org.elasticsearch.Version")
+      }
 
-            return false
-        }
+      println "executing $versionScript"
+      [
+          versionScript.absolutePath
+      ].execute([
+          "JAVA_HOME=${System.properties['java.home']}"
+      ], home).waitForProcessOutput(versionInfo, new StringBuffer())
+    } else {
+      File esScript = new File("${home}/bin/elasticsearch${isFamily(FAMILY_WINDOWS) ? '.bat' : ''}")
 
-        return true
+      [
+          esScript.absolutePath,
+          "-v"
+      ].execute([
+          "JAVA_HOME=${System.properties['java.home']}"
+      ], home).waitForProcessOutput(versionInfo, new StringBuffer())
     }
 
-    boolean isDesiredVersion() {
-        def versionInfo = new StringBuffer()
+    println "${CYAN}* elastic:$NORMAL: reported version: $versionInfo"
+    return versionInfo.contains(version)
+  }
 
-        println "${CYAN}* elastic:$NORMAL checking existing version"
-        if (isFamily(FAMILY_WINDOWS)) {
-            File versionScript = new File("${home}/bin/version.bat")
+  void install(List<String> withPlugins) {
+    println "${CYAN}* elastic:$NORMAL installing elastic version $version"
 
-            if (!versionScript.exists()) {
-                ant.copy(file: "${home}/bin/elasticsearch.bat", tofile: versionScript, filtering: true)
-                ant.replace(file: versionScript, token: "org.elasticsearch.bootstrap.Elasticsearch", value: "org.elasticsearch.Version")
-            }
+    String linuxUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz"
+    String winUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.zip"
 
-            println "executing $versionScript"
-            [
-                    versionScript.absolutePath
-            ].execute([
-                    "JAVA_HOME=${System.properties['java.home']}"
-            ], home).waitForProcessOutput(versionInfo, new StringBuffer())
-        } else {
-            File esScript = new File("${home}/bin/elasticsearch${isFamily(FAMILY_WINDOWS) ? '.bat' : ''}")
-
-            [
-                    esScript.absolutePath,
-                    "-v"
-            ].execute([
-                    "JAVA_HOME=${System.properties['java.home']}"
-            ], home).waitForProcessOutput(versionInfo, new StringBuffer())
-        }
-
-        println "${CYAN}* elastic:$NORMAL: reported version: $versionInfo"
-        return versionInfo.contains(version)
+    if (version.startsWith("5")) {
+      linuxUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}.tar.gz"
+      winUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}.zip"
+    } else if (version.startsWith("2")) {
+      linuxUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${version}/elasticsearch-${version}.tar.gz"
+      winUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/${version}/elasticsearch-${version}.zip"
     }
 
-    void install(List<String> withPlugins) {
-        println "${CYAN}* elastic:$NORMAL installing elastic version $version"
-        String linuxUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.tar.gz"
-        String winUrl = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${version}.zip"
+    String elasticPackage = isFamily(FAMILY_WINDOWS) ? winUrl : linuxUrl
+    File elasticFile = new File("$toolsDir/elastic-${version}.zip")
 
-        if (version.startsWith("2")) {
-            linuxUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${version}/elasticsearch-${version}.tar.gz"
-            winUrl = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/${version}/elasticsearch-${version}.zip"
-        }
+    DownloadAction elasticDownload = new DownloadAction(project)
+    elasticDownload.dest(elasticFile)
+    elasticDownload.src(elasticPackage)
+    elasticDownload.onlyIfNewer(true)
+    elasticDownload.execute()
 
-        String elasticPackage = isFamily(FAMILY_WINDOWS) ? winUrl : linuxUrl
-        File elasticFile = new File("$toolsDir/elastic-${version}.zip")
+    ant.delete(dir: home, quiet: true)
+    home.mkdirs()
 
-        DownloadAction elasticDownload = new DownloadAction(project)
-        elasticDownload.dest(elasticFile)
-        elasticDownload.src(elasticPackage)
-        elasticDownload.onlyIfNewer(true)
-        elasticDownload.execute()
-
-        ant.delete(dir: home, quiet: true)
-        home.mkdirs()
-
-        if (isFamily(FAMILY_WINDOWS)) {
-            ant.unzip(src: elasticFile, dest: "$home") {
-                cutdirsmapper(dirs: 1)
-            }
-        } else {
-            ant.untar(src: elasticFile, dest: "$home", compression: "gzip") {
-                cutdirsmapper(dirs: 1)
-            }
-            ant.chmod(file: new File("$home/bin/elasticsearch"), perm: "+x")
-            ant.chmod(file: new File("$home/bin/plugin"), perm: "+x")
-        }
-
-        if (withPlugins.contains("head plugin")) {
-            println "* elastic: installing the head plugin"
-            String plugin = "$home/bin/plugin"
-            if (isFamily(FAMILY_WINDOWS)) {
-                plugin += ".bat"
-            }
-
-            [
-                    new File(plugin),
-                    "--install",
-                    "mobz/elasticsearch-head"
-            ].execute([
-                    "JAVA_HOME=${System.properties['java.home']}",
-                    "JAVA_OPTS=${System.getenv("JAVA_OPTS")}",
-                    "ES_HOME=$home"
-
-            ], home)
-        }
+    if (isFamily(FAMILY_WINDOWS)) {
+      ant.unzip(src: elasticFile, dest: "$home") {
+        cutdirsmapper(dirs: 1)
+      }
+    } else {
+      ant.untar(src: elasticFile, dest: "$home", compression: "gzip") {
+        cutdirsmapper(dirs: 1)
+      }
+      ant.chmod(file: new File("$home/bin/elasticsearch"), perm: "+x")
+      ant.chmod(file: new File("$home/bin/plugin"), perm: "+x")
     }
+
+    if (withPlugins.contains("head plugin")) {
+      println "* elastic: installing the head plugin"
+      String plugin = "$home/bin/plugin"
+      if (isFamily(FAMILY_WINDOWS)) {
+        plugin += ".bat"
+      }
+
+      [
+          new File(plugin),
+          "--install",
+          "mobz/elasticsearch-head"
+      ].execute([
+          "JAVA_HOME=${System.properties['java.home']}",
+          "JAVA_OPTS=${System.getenv("JAVA_OPTS")}",
+          "ES_HOME=$home"
+
+      ], home)
+    }
+  }
 }
